2016e37f9ac15edfa2e646da56dc6a9d0e6167f4
Jaylon A <35003843+ja-423@users.noreply.github.com>
2024-07-04 12:22:19 -0400

    Angular theme and animations

--- cardselectordemo/src/app/app.component.ts
+++ cardselectordemo/src/app/app.component.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav'; 
+import { CommonModule } from "@angular/common";
+
+import { MatButtonModule} from '@angular/material/button'; 
+import { MatCardModule} from '@angular/material/card';
+import { MatExpansionModule} from '@angular/material/expansion';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatListModule} from '@angular/material/list';
-import { CommonModule } from "@angular/common";
 import { MatSelectModule} from '@angular/material/select'; 
-import { MatCardModule} from '@angular/material/card';
-import { MatExpansionModule} from '@angular/material/expansion'; 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule} from '@angular/material/input'; 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    MatSidenavModule,
-    MatFormFieldModule,
-    MatListModule,
     CommonModule,
-    MatSelectModule,
+    MatButtonModule,
     MatCardModule,
     MatExpansionModule,
+    MatFormFieldModule,
+    MatListModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSidenavModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
